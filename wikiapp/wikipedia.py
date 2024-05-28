f7990e60889574e1e532e6f7291ca057331f199d
SGGmilagro <120662824+SGGmilagro@users.noreply.github.com>
2024-05-28 14:28:53 -0400

    Formatting del mock

--- wikiapp/wikipedia.py
+++ wikiapp/wikipedia.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 import click
 import requests
 
 API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page(language="en"):
+def random_page(language: str = "en") -> Any:
     """Get a random page from Wikipedia"""
     try:
         with requests.get(API_URL.format(language=language), timeout=10) as response:
